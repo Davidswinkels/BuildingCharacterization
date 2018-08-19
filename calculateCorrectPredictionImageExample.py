a74370ee0fa97818f4bd85870424aa990f59fbb1
David <davidswinkelss@gmail.com>
2018-08-19 19:20:48 +0200

    Update prediction images

--- calculateCorrectPredictionImageExample.py
+++ calculateCorrectPredictionImageExample.py
@@ -17,6 +17,7 @@ buildingClasses = ['Residentia', 'Meeting', 'Industry', 'Office', 'Shop']
 
 fovs = ['F30', 'F60', 'F90', 'F30_60_90']
 randomImagePos = [3552, 5219, 19434, 20358, 14528, 9476, 8983, 8851, 6719, 6410]
+selectImageID = [6792, 10258, 114858, 4995, 5198, 12550, 83951, 94709, 5131, 14799, 5311, 21782, 68994, 17961, 44492]
 inputFilePath = './result/BuildingPointsPredictedLabels.csv'
 outputFileStem = './result/checkimage/CorrectPredictionImage'
 
@@ -27,20 +28,49 @@ predictedImages = pd.read_csv(inputFilePath)
 
 print(predictedImages.columns.values)
 
+# predictionData = {}
+# ## Append information from every model to csv file
+# for imagePos in randomImagePos:
+#   for buildingClass in buildingClasses:
+#     measuredValue = predictedImages[buildingClass].iloc[imagePos]
+#     predictionBuildingClassList = [measuredValue]
+#
+#     for fov in fovs:
+#       countCorrectPredictions = 0
+#       for iteration in iterations:
+#         predictedColumnName = 'pred_' + buildingClass + '_' + fov + '_' + str(iteration)
+#         print(predictedColumnName)
+#         predictedValue = predictedImages[predictedColumnName].iloc[imagePos]
+#         if predictedValue == measuredValue:
+#           countCorrectPredictions += 1
+#       predictionBuildingClassList.append(countCorrectPredictions)
+#     print(predictionBuildingClassList)
+#     predictionData[buildingClass] = predictionBuildingClassList
+#   predictionDF = pd.DataFrame(data=predictionData, columns = buildingClasses)
+#   buildingID = '_B' + str(predictedImages['BuildingID'].iloc[imagePos])
+#   panoID = '_P' + str(predictedImages['BuildingID'].iloc[imagePos])
+#   outputFilePath = outputFileStem + str(imagePos) + buildingID + panoID + '.csv'
+#   predictionDF.to_csv(outputFilePath)
+#   print(predictionDF.head())
+
+
 predictionData = {}
 ## Append information from every model to csv file
-for imagePos in randomImagePos:
+for posID in selectImageID:
+  imagePos = predictedImages.index[predictedImages['ID'] == posID].tolist()
   for buildingClass in buildingClasses:
-    measuredValue = predictedImages[buildingClass].iloc[imagePos]
+    measuredValue = int(predictedImages[buildingClass].iloc[imagePos])
     predictionBuildingClassList = [measuredValue]
+    print(measuredValue)
 
     for fov in fovs:
       countCorrectPredictions = 0
       for iteration in iterations:
         predictedColumnName = 'pred_' + buildingClass + '_' + fov + '_' + str(iteration)
         print(predictedColumnName)
-        predictedValue = predictedImages[predictedColumnName].iloc[imagePos]
-        if predictedValue == measuredValue:
+        predictedValue = int(predictedImages[predictedColumnName].iloc[imagePos])
+        print(predictedValue)
+        if int(predictedValue) == int(measuredValue):
           countCorrectPredictions += 1
       predictionBuildingClassList.append(countCorrectPredictions)
     print(predictionBuildingClassList)
@@ -48,10 +78,8 @@ for imagePos in randomImagePos:
   predictionDF = pd.DataFrame(data=predictionData, columns = buildingClasses)
   buildingID = '_B' + str(predictedImages['BuildingID'].iloc[imagePos])
   panoID = '_P' + str(predictedImages['BuildingID'].iloc[imagePos])
-  outputFilePath = outputFileStem + str(imagePos) + buildingID + panoID + '.csv'
+  outputFilePath = outputFileStem + str(posID) + buildingID + panoID + '.csv'
   predictionDF.to_csv(outputFilePath)
   print(predictionDF.head())
-
-
 
 
